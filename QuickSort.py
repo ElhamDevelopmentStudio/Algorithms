a4e1f5fff7970a482faa3f862fd5445461323cb4
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-03-11 22:29:21 +0430

    added quicksort

---
+++ QuickSort.py
@@ -0,0 +1,13 @@
+def quickSort(numbers):
+  if len(numbers) < 2:
+    return numbers
+
+  pivot = numbers[0]
+
+  smallerNumbers = [i for i in numbers[1:] if i <= pivot]
+  biggerNumbers = [i for i in numbers[1:] if i > pivot]
+
+  return quickSort(smallerNumbers) + [pivot] + quickSort(biggerNumbers)
+
+numbers = list(map(int, input().split()))
+print(quickSort(numbers))
